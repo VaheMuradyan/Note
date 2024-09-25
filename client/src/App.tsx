adf8bad6a839eb188f228092e7c1d8134ec93d30
vahe.muradyan.03 <vahe.muradyan.03@gamil.com>
2024-09-25 15:15:53 +0400

    working correct

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 import NoteForm from './components/NoteForm'
 import NoteList from './components/NoteList'
 
-export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+export const BASE_URL = "http://localhost:5000/api";
 
 function App() {
 
